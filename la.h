cf7c8eae1a5ee7e16f627e7ea4c792d2cce8da9d
Deepak Sangle <deepaksangleok@gmail.com>
2023-01-20 13:09:26 +0530

    CSV funtionality, error Handling and newline feature added

--- la.h
+++ la.h
@@ -7,11 +7,15 @@
 
 int curr_size = 0;
 int curr_max_size = INTIAL_ARRAY_SIZE;
+int line_nums = 1;
 char* buffer_pointer;
+int ScannerTerminated = 0;
 
 enum TokenKind { KEYWORDS, IDENTIFIERS, DELIMITORS, OPERATORS, LITERALS, STRINGS};
 
 char* TokenStrings[] = { "KEYWORDS", "IDENTIFIERS", "DELIMITORS", "OPERATORS", "LITERALS", "STRINGS"};
+char* LexicalErrors[] = {"String cannot contain multiple lines.", "Illegal character present.", "Bad excape sequence character present."};
+
 char string_buffer[STRING_BUFFER_SIZE];
 
 typedef struct{
@@ -55,7 +59,24 @@ void pushToken(enum TokenKind TokenType, char* lex){
     return ;
 }
 
-void showError(){
-    printf("Tokenization error present... Terminating Scanner!\n");
+void showError(char* temp, int errorCode){
+    ScannerTerminated = 1;
+    printf("Error at line num: %d\nError: %s\n", line_nums, LexicalErrors[errorCode]);
+    if(strcmp(temp, "")){
+        printf("Illegal Character present is: %c", temp);
+    }
+    return ;
+}
+
+void writeToCSV(){
+    printf("Number of distinct tokens: %d\n", curr_size);
+
+    FILE* fpt;
+    fpt = fopen("output.csv", "w+");
+    fprintf(fpt, "Lexeme, Token, Count\n");
+    for(int i=0;i<curr_size;i++){
+        fprintf(fpt, "\"%s\", %s, %d\n", Tokens[i].lexeme, TokenStrings[Tokens[i].TokenType], Tokens[i].count);
+    }
+    fclose(fpt);
     return ;
 }
