df3463fb036da7a248c411ae2525f5130d3bc470
Shreyasi Mandal <shreyasi18sia@gmail.com>
2023-03-23 10:09:21 +0530

    ..

--- src/test.java
+++ src/test.java
@@ -1,41 +1,20 @@
 public class test {
-    int x, y;
-    test t;
-    // {
-    //     int x;
-    // }
+    int x;
+    // test t;
     // test(int x){
 
     // }
-    public void main() {
-        x = 1;
-        test t4;
-        for (int i; i < 5; i++) {
-            for (int j; j < 5; j++) {
-                int deepu_khi_khi;
-            }
-            int wer;
-        }
-        if (x == 3) {
-            int z;
-        }
-        int qw[][];
-        x = qw[1][2];
+    public void main(){
+        // test t2 = new test();
+        int y = 5;
+        // x = y * 6;
+        // for(x++;x<5;x--){
 
-    }
+        // }
+        // if(x){
 
-    public void doosra() {
-        x = 1;
-        test t7;
-        for (int ii; ii < 5; ii++) {
-            for (int jj; jj < 5; jj++) {
-                int deepu_khi_khi_ghgfhgfhgf;
-            }
-            int were;
-        }
-        if (x == 3) {
-            int zz;
-        }
-
+        // }
+        // x = 5 + 6 + y;
+        // test t4;
     }
 }
