e9565127d32969ac43448c12b5a0298525b3d5e1
Shreyasi Mandal <shreyasi18sia@gmail.com>
2023-03-24 15:42:23 +0530

    .

--- src/test.java
+++ src/test.java
@@ -6,27 +6,27 @@ class test {
     // double fivepointfive(){return 5.5;}
     
     public void main(){
-        // test t2 = new test();
-        int y = 5;
-        y = x*6 + x*2 + x*3;
-        // y = 5.5;
-        // if(x == 5){
-        //     // y = 6;
-        // }
-        // // int c = test;
-        // int b = x*6;
-        // test t = new test();
-        // // t.s = 5;
-        // char x = 5;
-        // boolean a = true;
-        // b = 6;
-        // if(b==5){
+    test t2 = new test();
+    int y = 5;
+    y = x * 6 + x * 2 + x * 3;
+    double y1 = 5.5;
+    if(x == 5){
+        // y = 6;
+    }
+    // int c = test;
+    int b = x*6;
+    test t = new test();
+    // t.s = 5;
+    char x = 5;
+    boolean a = true;
+    b = 6;
+    if(b==5){
 
-        // }
-        // int y = 5;
-        // x = 6;
-        // x = 5;
-        // for(x++;x<5;x--){}
+    }
+    int y = 5;
+    x = 6;
+    x = 5;
+    for(x++;x<5;x--){}
 
     }
 }
