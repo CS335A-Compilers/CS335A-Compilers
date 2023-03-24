87cd2cdb60744df32a72d3b00b3b0db21fd9a853
Shreyasi Mandal <shreyasi18sia@gmail.com>
2023-03-24 17:14:58 +0530

    Fixed double to float type casting

--- src/test.java
+++ src/test.java
@@ -4,35 +4,55 @@ class test {
     // }
 
     // double fivepointfive(){return 5.5;}
-    
-    public void main(){
-    test t2 = new test();
-    int y = 5;
-    y = x * 6 + x * 2 + x * 3;
-    double y1 = 5.5;
-    if(x == 5){
-        // y = 6;
-    }
-    // int c = test;
-    int b = x*6;
-    test t = new test();
-    // t.s = 5;
-    char x = 5;
-    boolean a = true;
-    b = 6;
-    if(b==5){
 
-    }
-    int y = 5;
-    x = 6;
-    x = 5;
-    for(x++;x<5;x--){}
-
-    }
-
-    int x7;
-    {
-        x7 = 0;
-        // x7 = 6 > 5 ? 0 : 1;
+    public void main() {
+        // test t2 = new test();
+        int y = 5;
+        y = x * 6 + x * 2 + x * 3;
+        byte a = 9;
     }
 }
+
+// //     if(x == 5){
+// //         // y = 6;
+// //     }
+// //     // int c = test;
+// //     int b = x*6;
+// //     // test t = new test();
+// //     // t.s = 5;
+// //     char x = 5;
+// //     boolean a;
+// //     a = true || false;
+// //     b = 6;
+// //     if(b==5){
+
+// //     }
+// //     int y = 5;
+// //     x = 6;
+// //     x = 5;
+// //     for(x++;x<5;x--){}
+
+// //     }
+
+// //     int x7;
+// //     {
+// //         x7 = 0;
+// //         // x7 = 6 > 5 ? 0 : 1;
+// //     }
+// // }
+// class test {
+    
+//     public void main(){
+     
+//         boolean b=true||false;
+//         boolean k,l,m;
+//         k=true||true;
+//         l=true&&false;
+//         m=false&&true;
+  
+//     }
+    
+
+        
+  
+//     }
