c327370b99ad8ac0026b2bc6bbc5a8f67a133d55
Vartika001 <vartika2571@gmail.com>
2023-03-24 21:57:49 +0530

    Test case check

--- src/test.java
+++ src/test.java
@@ -1,19 +1,14 @@
-public class best_class {
-    public void main() {
-        
-        byte b = 8;        
-        short s = -327-b; 
-        int i = 2147483647*1; 
-        long l = -9223372036854775808L; 
-        boolean f = 3.4028235E38f<776; 
-        boolean b=45>8;
-        double d = 1.5848-b; 
-        // char c = 'Z';
-        boolean bool = false;
-        
-        
-        
-       
-    }
+
+class arithmetic {
     
-}
+    
+    public void main(){
+       
+        int k=+7;
+        double g=-2.3;
+        int a=-8;
+        // float f=6+7.1;
+        double d=+6.556d;
+  
+    }
+  }
