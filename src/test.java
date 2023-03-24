626e626d43d754f21cf2ac5128f9519028f1cdfa
Shreyasi Mandal <shreyasi18sia@gmail.com>
2023-03-24 15:38:13 +0530

    ..

--- src/test.java
+++ src/test.java
@@ -1,27 +1,37 @@
 class test {
     
     int x = 0;
-    public void main(){
+
+    public void main() {
         // test t2 = new test();
-        int y = 5.5;
-        // y = 5.5;
-        // if(x == 5){
-        //     y *= 6;
-        // }
+        int y;
+        y = 5;
+        // x = 6 > 5 ? 0 : 1;
+        // x = 6 == 5 ? 0 : 1;
+        if (x == 5) {
+            y *= 6;
+        }
         // int c = test;
-        // int b = x*6;
+        int b = x * 6;
         // test t = new test();
         // t.s = 5;
-        // char x = 5;
-        // boolean a = true;
-        // b = 6;
-        // if(b==5){
+        char x = 5;
+        boolean a = 8 > 0;
+        b = 6;
+        if (b == 5) {
 
-        // }
-        // int y = 5;
-        // x = 6;
-        // x = 5;
-        // for(x++;x<5;x--){}
+        }
+        int y = 5;
+        x = 6;
+        x = 5;
+        for (x++; x < 5; x--) {
+        }
 
+    }
+
+    int x7;
+    {
+        x7 = 0;
+        // x7 = 6 > 5 ? 0 : 1;
     }
 }
