4e1b13b2c5db89a59b4d028966f45eb9ef67962f
Vartika001 <vartika2571@gmail.com>
2023-03-24 15:49:13 +0530

    Added test cases

--- src/test.java
+++ src/test.java
@@ -1,27 +1,12 @@
+
 class test {
     
-    int x = 0;
-    public void main(){
-        // test t2 = new test();
-        int y = 5.5;
-        // y = 5.5;
-        // if(x == 5){
-        //     y *= 6;
-        // }
-        // int c = test;
-        // int b = x*6;
-        // test t = new test();
-        // t.s = 5;
-        // char x = 5;
-        // boolean a = true;
-        // b = 6;
-        // if(b==5){
+    boolean b=true||false;
+    boolean k;
+    k=true||true;
+    // l=true && false;
+    // m=false && true;
 
-        // }
-        // int y = 5;
-        // x = 6;
-        // x = 5;
-        // for(x++;x<5;x--){}
-
+        
+  
     }
-}
