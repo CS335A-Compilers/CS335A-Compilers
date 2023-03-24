b5b2f3316b3b2e10c782e863ead521c9d9fa1854
Vartika001 <vartika2571@gmail.com>
2023-03-24 23:20:35 +0530

    Test case added

--- src/test.java
+++ src/test.java
@@ -5,14 +5,15 @@ class test {
     public void main(){
        
         boolean y ;
+        int x;
+        double a,f;
         y=6<7;
-        int x=5==7?5:9;
-        double a=false? 9.6d:2.3;
-        //int k=4>5?2:5;
-        //char s=true?'a':'b'; Segmentation fault (core dumped)
-        double f=true ? 5.6f:6; //error when float f=true ? 5.6f:6;
-        boolean b;
-        b=true?true:false;
+        x=4*3;
+        if(x==12)
+        {
+            x++;
+        }
+        x--;
         
   
     }
